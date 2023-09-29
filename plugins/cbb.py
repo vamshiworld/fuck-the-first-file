3d612936c03bebf64522b3a20e27923a2e1960db
vamshiworld <128972919+vamshiworld@users.noreply.github.com>
2023-09-29 19:33:20 +0600

    cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ My Owner : @Aravind_maharaj6\n○Channel : @world_of_cartoons\n○ Support Group : @world_of_cartoons</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
@@ -22,6 +22,10 @@ async def cb_handler(client: Bot, query: CallbackQuery):
         )
     elif data == "close":
         await query.message.delete()
+        try:
+            await query.message.reply_to_message.delete()
+        except:
+            pass
         try:
             await query.message.reply_to_message.delete()
         except:
